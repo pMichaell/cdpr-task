8840023315a1901b1eecbe77bf14c2427130a0b1
pMichaell <michal.pawlowicz112@gmail.com>
2022-10-29 18:39:23 +0200

    project setup in progress

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["plugin:react/recommended", "xo", "prettier"],
+  overrides: [
+    {
+      extends: ["xo-typescript", "prettier"],
+      files: ["*.ts", "*.tsx"],
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["prettier", "react"],
+  rules: {
+    "prettier/prettier": "error",
+    "react/react-in-jsx-scope": "off",
+    "arrow-body-style": "off",
+  },
+};
---
+++ webpack.config.renderer.dev.js
@@ -0,0 +1,65 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+const ReactRefreshTypeScript = require("react-refresh-typescript");
+
+module.exports = {
+  mode: "development",
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  entry: path.resolve(__dirname, "/src/renderer/index.tsx"),
+  output: {
+    filename: "[name].[contenthash].js",
+    clean: true,
+    path: path.resolve(__dirname, "dist"),
+  },
+  devtool: "source-map",
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\ts$/,
+        exclude: /node_modules/,
+        use: "ts-loader",
+      },
+      {
+        test: /\.[jt]sx$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: require.resolve("ts-loader"),
+            options: {
+              getCustomTransformers: () => ({
+                // eslint-disable-next-line new-cap
+                before: [ReactRefreshTypeScript()],
+              }),
+              transpileOnly: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Typescript React App",
+      filename: "index.html",
+      template: "src/renderer/index.html",
+    }),
+    new ReactRefreshWebpackPlugin(),
+  ],
+};
