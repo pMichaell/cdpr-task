dc795eb57243f1a89b9a1cf17aec129780e6581e
pMichaell <michal.pawlowicz112@gmail.com>
2022-11-01 22:19:42 +0100

    scripts updated

--- webpack.config.main.js
+++ webpack.config.main.js
@@ -1,17 +1,22 @@
 const path = require('path');
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
 module.exports = {
-  mode: 'development',
+  mode: process.env.NODE_ENV,
   target: 'electron-main',
   resolve: {
     extensions: ['.ts'],
+  },
+  optimization: {
+    nodeEnv: isDevelopment ? 'development' : 'production',
   },
   entry: {
     main: path.resolve(__dirname, 'src', 'main', 'main.ts'),
     preload: path.resolve(__dirname, 'src', 'main', 'preload.ts'),
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
     filename: '[name].js',
+    path: path.resolve(__dirname, 'dist', 'main'),
     clean: true,
   },
   devtool: 'source-map',
